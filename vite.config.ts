c063afe1f4388eada2dff9ccb4052b7ae42a886e
Myung Jun Lee <promise4039@gmail.com>
2026-02-25 22:49:43 +0900

    Add GitHub Actions Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // If deploying to GitHub Pages, set base to '/<repo-name>/'
-  // base: '/your-repo-name/',
+  base: '/gagebu/',
 })
